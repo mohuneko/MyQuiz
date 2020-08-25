454c3bfd27d8326b7067a485ad2b77026f44d8a3
mohuneko <b0wdmohu@gmail.com>
2020-08-25 20:11:06 +0900

    add

--- js/main.js
+++ js/main.js
@@ -12,8 +12,9 @@
      {q: 'What is C?', c: ['C0', 'C1', 'C2']},
    ];
    let currentNum = 0;
+   let isAnswered;
+   let score = 0;
 
-   question.textContent = quizSet[currentNum].q;
 
    function shuffle(arr) {
       for (let i = arr.length - 1; i > 0; i--) {
@@ -23,9 +24,59 @@
     return arr;
    }
 
-   quizSet[currentNum].c.forEach(choice => {
-    const li = document.createElement('li');
-    li.textContent = choice;
-    choices.appendChild(li);
-   });
+   function checkAnswer(li) {
+     if (isAnswered) {
+       return;
+     }
+     isAnswered = true;
+
+     if (li.textContent === quizSet[currentNum].c[0]) {
+       li.classList.add('correct');
+       score++;
+     } else {
+      li.classList.add('wrong');
+     }
+
+     btn.classList.remove('disabled');
+   }
+
+  function setQuiz() {
+    isAnswered = false;
+
+    question.textContent = quizSet[currentNum].q;
+
+    while(choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+
+    const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+    shuffledChoices.forEach(choice => {
+     const li = document.createElement('li');
+     li.textContent = choice;
+     li.addEventListener('click', () => {
+       checkAnswer(li);
+     });
+     choices.appendChild(li);
+    });
+
+    if (currentNum === quizSet.length - 1) {
+      btn.textContent = 'Show Score';
+    }
+  }
+
+  setQuiz();
+
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('disabled')) {
+      return;
+    }
+    btn.classList.add('disabled');
+
+    if (currentNum === quizSet.length - 1) {
+      console.log(`Score: ${score} / ${quizSet.length}`);
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
 }
